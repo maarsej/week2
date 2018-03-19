7138a69a465adfbcb90b4ff56735bc2768148f8c
Nucleartom <jacob.maarse@gmail.com>
2018-03-19 15:24:41 +0000

    Added callback to define what the html request does on end

---
+++ day1/printcallbacks.js
@@ -0,0 +1,32 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    var buffer ="";
+    response.on('data', function(chunk){
+      buffer += chunk;
+    })
+
+    response.on('end', function() {
+      callback(buffer);
+    })
+  })
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+function saveHTML(html){
+  return html;
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
